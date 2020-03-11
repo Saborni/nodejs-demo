4fcd6898711dc49d38a433fd0ffe4da1e28a60e4
Saborni Bhattacharya <saborni.bhattacharya@accenture.com>
2020-03-11 21:07:50 +0530

    Changes in header

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var fs = require('fs');
 function getProds(req,res){
     fs.readFile('products.json', function(err, data) {
         if (err) throw err;
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.write(data);
+        //res.writeHead(200, {'Content-Type': 'application/json',});
+        res.header("Content-Type", "application/json; charset=utf-8");
+        res.write(JSON.parse(data));
         //console.log(data);
         res.end();
         });
